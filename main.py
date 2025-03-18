0cad121251814a0ba4908ad6157affcaa6ed9255
Daniel Steven <dan.p.steven@gmail.com>
2025-03-17 21:55:31 -0400

    initial commit

--- main.py
+++ main.py
@@ -1,22 +1,6 @@
 import pandas as pd
 
-output_file_location = './data/yelp_review_dataset.csv'
-input_file_location = './data/yelp_academic_dataset_review.json'
-chunk_size = 10000
-
-def get_sentiment(stars):
-    if stars <= 2 :
-        return 'negative'
-    elif stars == 3:
-        return 'neutral'
-    else:
-        return 'positive'
-
 def main():
-
-
-    df = pd.read_csv('data/yelp_review_dataset.csv')
-
 
 
 
