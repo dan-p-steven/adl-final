8bd38f6492bb786e66e653cfd5d999f8e41fbf19
Daniel Steven <n01715077@humber.ca>
2025-03-30 11:39:21 -0400

    generate word embeddings

--- main.py
+++ main.py
@@ -1,52 +1,8 @@
 import pandas as pd
-import spacy
-from tqdm import tqdm
-import time
 
-spacy_nlp = spacy.load('en_core_web_lg')
-
-IN_DATA_PATH = './data/yelp_review_100k.csv'
-OUT_DATA_PATH = './data/vectorized_yelp_dataset_100k.parquet'
-
-CHUNKSIZE = 10000
 
 def main():
-    df_iter = pd.read_csv(IN_DATA_PATH, chunksize=CHUNKSIZE)
-
-    for i, df in enumerate(df_iter):
-    
-        # Read Dataset
-        X = df['text']
-        y = df['sentiment'].map({'negative': 0, 'neutral': 1, 'positive': 2})
-
-        # Create word vectors using Spacy
-        texts = X.tolist()
-        
-        # Create a list to store token word vectors with ownership information (sample identification)
-        tokenized_vectors = []
-
-        start = time.time()
-
-        batch_size=10
-        disable = ['ner', 'parser', 'attribute_ruler', 'lemmatizer', 'tagger']
-        print (f'Chunk {i}:')
-        for doc in tqdm(spacy_nlp.pipe(texts, 
-                                batch_size=batch_size, 
-                                disable=disable, 
-                                n_process=1)):
-            sentence = []
-            for token in doc:
-                if token.has_vector:
-                    sentence.append(token.vector)
-            tokenized_vectors.append(sentence)
-        end = time.time()
-
-
-        print (f'{end-start:.2f}s')
-        df['text_vectorized'] = tokenized_vectors
-        df.to_parquet(f'./data/vectorized_chunk_{i}.parquet', index=False)
-
-
+    pass
 
 if __name__ == "__main__":
     main()
---
+++ src/preprocess.py
@@ -0,0 +1,67 @@
+import pandas as pd
+import spacy
+from tqdm import tqdm
+import time
+
+# Load Spacy's NLP model to use for in vector embeddings
+# spacy_nlp = spacy.load('en_core_web_lg')
+
+CHUNKSIZE = 10000
+
+def generate_save_word_embeddings(dataset_read_path, dataset_save_path):
+    '''
+    Read the Yelp Review dataset and vectorize them using SpaCy's GloVe model.
+    Save the model in .parquet format to a desired location.
+
+    Input:  dataset_read_path
+                Location of the .csv file containing reviews.
+            dataset_save_path
+                Location of the .parquet file containing vectors you want to save.
+    Output: None.
+    '''
+
+    # Read the dataset 10 000 entries at a time and generae vectors for them
+    df_iter = pd.read_csv(dataset_read_path, chunksize=CHUNKSIZE)
+
+    for i, df in enumerate(df_iter):
+
+        # Convert dataframe to a list.
+        texts = df['text'].tolist()
+        
+        # List to store tokenized vectors.
+        tokenized_vectors = []
+
+        # Start time.
+        start = time.time()
+
+        
+        print (f'Chunk {i}:') 
+
+        # Disables parts of the NLP pipeline that are not used.
+        disable = ['ner', 'parser', 'attribute_ruler', 'lemmatizer', 'tagger']
+
+        batch_size = 10
+
+        # Tokenize sentences.
+        for doc in tqdm(spacy_nlp.pipe(texts, 
+                                batch_size=batch_size, 
+                                disable=disable, 
+                                n_process=1)):
+            # Record the tokens that have a vector.
+            sentence = []
+            for token in doc:
+                if token.has_vector:
+                    sentence.append(token.vector)
+
+            # Append the vectorized sample.
+            tokenized_vectors.append(sentence)
+
+        # End time.
+        end = time.time()
+
+
+        print (f'{end-start:.2f}s')
+
+        # Save the data in .parquet format, an efficient file read/write format.
+        df['text_vectorized'] = tokenized_vectors
+        df.to_parquet(dataset_save_path, index=False)
